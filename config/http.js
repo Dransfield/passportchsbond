971d91f77664fdb1c2c73916c901b2299a88e5ce
root <slenkar@gmail.com>
2016-12-06 13:08:59 +0800

    h

--- config/http.js
+++ config/http.js
@@ -39,7 +39,7 @@ module.exports.http = {
             'passportInit',     
             'passportSession',
             'heck', 
-            'myRequestLogger',
+           
             'bodyParser',
             'handleBodyParserError',
             'compress',
